56ca856720cd8efa0fbd3dddeb3bdb0851be499f
Mario Cruz <cruzperezmario802@gmail.com>
2023-10-04 20:06:56 -0600

    04/10/23 MECP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OperasBasComponent } from './formulario/operas-bas/operas-bas.component';
@@ -11,20 +8,25 @@ import { FormsModule } from '@angular/forms';
 import { MultiplicacionAXBComponent } from './formulario/multiplicacion-axb/multiplicacion-axb.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material/material/material.module';
+import { MenuComponent } from './menu/menu/menu.component';
+import { CinepolisComponent } from './formularios/cinepolis/cinepolis.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     OperasBasComponent,
     MultiplicacionAXBComponent,
+    MenuComponent,
+    CinepolisComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatButtonModule,
-    MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formularios/cinepolis/cinepolis.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinepolis',
+  templateUrl: './cinepolis.component.html',
+  styleUrls: ['./cinepolis.component.css']
+})
+export class CinepolisComponent {
+
+  name:string='';
+  cant_comp:number=1;
+  cineco:boolean=true;
+  cant_bolt:number=0;
+
+  costo_boleto:number=12;
+  total:number=0;
+
+  descuentosText:string='';
+  descuentosCText:string='';
+
+  tieneCineco(hasCineco:boolean):void{
+    this.cineco = hasCineco;
+    console.log("He/she has CINECO? " + this.cineco);
+  }
+
+  procesar():void{
+    this.descuentosText = '';
+    this.descuentosCText = '';
+    this.total = 0;
+    if(this.cant_comp/this.cant_bolt >= 1/7){
+
+      if(this.cant_bolt > 2){
+        this.descuentos();
+      }else{
+        this.total = this.costo_boleto * this.cant_bolt;
+      }
+
+      if(this.cineco){
+        this.descuentoCineco();
+      }
+
+    }else{
+      this.descuentosText = "No se puede hacer la transacción: "
+      + this.cant_comp + " persona no pueden comprar "
+      + this.cant_bolt + " boletos."
+    }
+  }
+
+  descuentos():void{
+    let costo_inicial:number=this.costo_boleto * this.cant_bolt;
+    let descuento:number=0;
+
+    if(this.cant_bolt > 5){
+      descuento = 15 * costo_inicial / 100;
+      this.total = costo_inicial - descuento;
+    }else{
+      descuento = 10 * costo_inicial / 100;
+      this.total = costo_inicial - descuento;
+    }
+
+    this.descuentosText = 'Descuento ' + this.cant_bolt + ' boletos comprados: -$' + descuento;
+  }
+
+  descuentoCineco():void{
+    let costo_inicial:number=this.costo_boleto * this.cant_bolt;
+    let descuento:number = 10 * costo_inicial / 100;
+    this.total -= descuento;
+
+    this.descuentosCText += "Descuento CINECO aplicado: -$" + descuento; 
+  }
+
+}
